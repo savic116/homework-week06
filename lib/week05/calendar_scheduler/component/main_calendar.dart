7a58e38af27439b096351bdcb28ef8d6b8a242b3
savic116 <savic00085@gmail.com>
2025-10-11 00:14:55 +0900

    feat: Implement basic MainCalendar widget structure

--- lib/week05/calendar_scheduler/component/main_calendar.dart
+++ lib/week05/calendar_scheduler/component/main_calendar.dart
@@ -1,2 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:table_c';
+import 'package:table_calender/table_calender.dart';
+
+class MainCalendar extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
