82ca00c6def61c9caf3ddf15c8f30383c5718478
savic116 <savic00085@gmail.com>
2025-10-11 00:18:25 +0900

    feat: Integrate MainCalendar into HomeScreen

--- lib/week05/calendar_scheduler/screen/home_screen.dart
+++ lib/week05/calendar_scheduler/screen/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:myapp/week05/calendar_scheduler/component/main_calendar.dart';
 
 class HomeScreen extends StatelessWidget{
   const HomeScreen({Key? key}) : super (key: key);
@@ -6,7 +7,13 @@ class HomeScreen extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('Home Screen'),
+      body: SafeArea(
+        child: Column(
+          children: [
+            MainCalendar(),
+          ],
+        ),
+      ),
     );
   }
 }
