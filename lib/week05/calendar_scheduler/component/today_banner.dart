89b0393acb6f9688b55ab1bcba704164daf2687c
savic116 <savic00085@gmail.com>
2025-10-11 02:29:29 +0900

    feat: Display selected date in TodayBanner

--- lib/week05/calendar_scheduler/component/today_banner.dart
+++ lib/week05/calendar_scheduler/component/today_banner.dart
@@ -14,7 +14,24 @@ class TodayBanner extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     final textStyle = TextStyle(
-      font
+      fontWeight: FontWeight.w600,
+      color: Colors.white,
+    );
+
+    return Container(
+      color: PRIMARY_COLOR,
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+        child:  Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              '${selectedDate.year}년 ${selectedDate.month}월 ${selectedDate.day}일',
+              style: textStyle,
+            )
+          ],
+        ),
+      )
     )
   }
 }
