be277c3dcdee94028221a1970ee23612f5e4c402
savic116 <savic00085@gmail.com>
2025-10-11 01:58:58 +0900

    feat(schedule_card): Implement basic _Time widget structure

--- lib/week05/calendar_scheduler/component/schedule_card.dart
+++ lib/week05/calendar_scheduler/component/schedule_card.dart
@@ -4,5 +4,15 @@ import 'package:flutter/material.dart';
 class _Time extends StatelessWidget{
   final int startTime;
   final int endTime;
-  
+
+  const _Time({
+    required this.startTime,
+    required this.endTime,
+    Key? key,
+  }) : super(key: key);
+
+@override
+  Widget build(BuildContext context) {
+    
+  }
 }
