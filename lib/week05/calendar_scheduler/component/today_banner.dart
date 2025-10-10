df9975e19d3a8cf2cad6c56a89dbb28e82cc4262
savic116 <savic00085@gmail.com>
2025-10-11 02:38:56 +0900

    Fix: Corrected typo in TodayBanner's selectedDate type

--- lib/week05/calendar_scheduler/component/today_banner.dart
+++ lib/week05/calendar_scheduler/component/today_banner.dart
@@ -2,7 +2,7 @@ import 'package:myapp/week05/calendar_scheduler/const/colors.dart';
 import 'package:flutter/material.dart';
 
 class TodayBanner extends StatelessWidget{
-  final DataTime selectedDate; // 선택된 날짜
+  final DateTime selectedDate; // 선택된 날짜
   final int count; // 일정 개수
 
   const TodayBanner({
@@ -35,7 +35,7 @@ class TodayBanner extends StatelessWidget{
             )
           ],
         ),
-      )
-    )
+      ),
+    );
   }
 }
