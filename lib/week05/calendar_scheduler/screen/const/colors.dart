e684b7e12e42fcae00593d9f17326f282e7e3ec6
savic116 <savic00085@gmail.com>
2025-10-11 00:11:55 +0900

    Fix: Corrected duplicate LIGHT_GREY_COLOR definition.

--- lib/week05/calendar_scheduler/screen/const/colors.dart
+++ lib/week05/calendar_scheduler/screen/const/colors.dart
@@ -2,4 +2,5 @@ import 'package:flutter/material.dart';
 
 const PRIMARY_COLOR = Color(0xFF0DB2B2);
 final LIGHT_GREY_COLOR = Colors.grey[200]!;
-f
+final DARK_GREY_COLOR = Colors.grey[600]!;
+final LIGHT_GREY_COLOR = Colors.grey[200]!;
