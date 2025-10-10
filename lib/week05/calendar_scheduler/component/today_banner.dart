9f7a9513ed3b98a9e607d6d128eeda45549a5432
savic116 <savic00085@gmail.com>
2025-10-11 02:25:29 +0900

    feat: Implement TodayBanner component

---
+++ lib/week05/calendar_scheduler/component/today_banner.dart
@@ -0,0 +1,19 @@
+import 'package:myapp/week05/calendar_scheduler/const/colors.dart';
+import 'package:flutter/material.dart';
+
+class TodayBanner extends StatelessWidget{
+  final DataTime selectedDate; // 선택된 날짜
+  final int count; // 일정 개수
+
+  const TodayBanner({
+    required this.selectedDate,
+    required this.count,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
