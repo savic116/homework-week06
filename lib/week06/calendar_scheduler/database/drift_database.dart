ee2c36292e627bfbe5f712d3cf3cc0b5aa227ac8
savic116 <savic00085@gmail.com>
2025-10-11 15:38:56 +0900

    feat(drift_database): Import necessary dependencies for database setup

--- lib/week06/calendar_scheduler/database/drift_database.dart
+++ lib/week06/calendar_scheduler/database/drift_database.dart
@@ -1,6 +1,11 @@
 import 'package:myapp/week06/calendar_scheduler/model/schedule.dart';
 import 'package:drift/drift.dart';
 
+import 'package:drift/native.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart' as p;
+import 'dart:io';
+
 // private 값까지 불러올 수 있음
 part 'drift_database.g.dart'; // part 파일 지정
 
