2bf9051deb370f083b4af1439fd4e29acc1349cd
savic116 <savic00085@gmail.com>
2025-10-11 15:11:04 +0900

    feat: Enable scrollable, full-height ScheduleBottomSheet

--- lib/week05/calendar_scheduler/screen/home_screen.dart
+++ lib/week05/calendar_scheduler/screen/home_screen.dart
@@ -30,6 +30,9 @@ Widget build(BuildContext context) {
           context: context,
           isDismissible: true,
           builder: (_) => ScheduleBottomSheet(),
+          // BottomSheet의 높이를 화면의 최대 높이로
+          // 정의하고 스크롤 가능하게 변경
+          isScrollControlled: true,
         );
       },
       child: Icon(
