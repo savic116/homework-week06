0922a18d1b366d270512d2a7e14bf05476ce2432
savic116 <savic00085@gmail.com>
2025-10-11 13:53:03 +0900

    Fix: Remove unused import 'package:fff'gg

--- lib/week05/calendar_scheduler/screen/home_screen.dart
+++ lib/week05/calendar_scheduler/screen/home_screen.dart
@@ -2,8 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:myapp/week05/calendar_scheduler/component/main_calendar.dart';
 import 'package:table_calendar/table_calendar.dart';
 import 'package:myapp/week05/calendar_scheduler/component/schedule_card.dart';
-import
-
+import 'package:fff'gg
 class HomeScreen extends StatefulWidget{
   const HomeScreen({Key? key}) : super (key: key);
 
