9c5fb80aa5e33eaacef699f13e0d13a544fab6fe
savic116 <savic00085@gmail.com>
2025-10-11 14:08:33 +0900

    feat(calendar_scheduler): Implement basic CustomTextField component

---
+++ lib/week05/calendar_scheduler/component/custom_text_field.dart
@@ -0,0 +1,16 @@
+import 'package:myapp/week05/calendar_scheduler/const/colors.dart';
+import 'package:flutter/material.dart';
+
+class CustomTextField extends StatelessWidget{
+  final String label;
+
+  const CustomTextField({
+    required this.label,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column();
+  }
+}
