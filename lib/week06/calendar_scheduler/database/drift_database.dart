75e02ad61d3529145ccd47eeb07864e3ca65b612
savic116 <savic00085@gmail.com>
2025-10-11 15:30:26 +0900

    feat: Implement schedule watching by date

--- lib/week06/calendar_scheduler/database/drift_database.dart
+++ lib/week06/calendar_scheduler/database/drift_database.dart
@@ -11,4 +11,7 @@ part 'drift_database.g.dart'; // part 파일 지정
 )
 class LocalDatabase extends _$LocalDatabase {
   // code generation으로 생성할 클래스 상속
+  Stream<List<Schedules>> watchSchedules(DateTime data) =>
+  // 데이터를 조회하고 변화 감지
+     (select(Schedules)..where((tbl) => tbl.data.equals(data))).watch();
 }
