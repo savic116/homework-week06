d794e5b027b3ed4e1831a83c13f0c09bab5e34f3
savic116 <savic00085@gmail.com>
2025-10-11 13:58:34 +0900

    feat: Implement basic ScheduleBottomSheet structure

---
+++ lib/week05/calendar_scheduler/component/schedule_bottom_sheet.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ScheduleBottomSheet extends StatefulWidget{
+  const ScheduleBottomSheet({Key? key}) : super(key: key);
+
+  @override
+  StatefulElement createElement() {
+    // TODO: implement createElement
+    return super.createElement();
+  }
+}
