1a6b7801edcfc384583dca86a53cf5cd5b6aa7ed
savic116 <savic00085@gmail.com>
2025-10-11 15:14:04 +0900

    feat: Initialize date formatting and run HomeScreen

--- lib/week05/calendar_scheduler/main.dart
+++ lib/week05/calendar_scheduler/main.dart
@@ -4,5 +4,13 @@ import 'package:intl/date_symbol_data_local.dart';
 
 void main() async {
   // 플러터 프레임워크가 준비될 때까지 대기
-  WidgetFlu
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await initializeDateFormatting(); // intl 패키지 초기화(다국어화)
+
+  runApp(
+    MaterialApp(
+      home: HomeScreen(),
+    ),
+  );
 }
