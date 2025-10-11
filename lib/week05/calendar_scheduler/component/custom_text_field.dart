0a9d5e482cb3058d436182287e1f648d9d321a2d
savic116 <savic00085@gmail.com>
2025-10-11 14:48:04 +0900

    feat(calendar_scheduler): Add isTime property to CustomTextField

--- lib/week05/calendar_scheduler/component/custom_text_field.dart
+++ lib/week05/calendar_scheduler/component/custom_text_field.dart
@@ -2,7 +2,8 @@ import 'package:myapp/week05/calendar_scheduler/const/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 class CustomTextField extends StatelessWidget{
-  final String label;
+  final String label; // 텍스트 필드 제목
+  final bool isTime; // 시간 선택하는 텍스트 필드인지 여부
 
   const CustomTextField({
     required this.label,
