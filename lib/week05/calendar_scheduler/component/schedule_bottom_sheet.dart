cb2cf8e37560e9a953523c2f1b169b1e03f041e2
savic116 <savic00085@gmail.com>
2025-10-11 15:08:04 +0900

    Fix: Adjust bottom sheet height for keyboard visibility

--- lib/week05/calendar_scheduler/component/schedule_bottom_sheet.dart
+++ lib/week05/calendar_scheduler/component/schedule_bottom_sheet.dart
@@ -15,7 +15,7 @@ class _ScheduleBottomSheetState extends State<ScheduleBottomSheet>{
     final bottomInset = MediaQuery.of(context).viewInsets.bottom;
     return SafeArea(
       child: Container(
-        height: MediaQuery.of(context).size.height / 2,
+        height: MediaQuery.of(context).size.height / 2 + bottomInset,
         color: Colors.white,
         child: Padding(
           padding: const EdgeInsets.only(left: 8, right: 8, top: 8),
