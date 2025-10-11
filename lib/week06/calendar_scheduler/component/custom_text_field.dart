b10a4f5408f2ab179ec99ee4aa2bc8811332a6e9
savic116 <savic00085@gmail.com>
2025-10-11 15:50:27 +0900

    feat: Add form validation to CustomTextField

--- lib/week06/calendar_scheduler/component/custom_text_field.dart
+++ lib/week06/calendar_scheduler/component/custom_text_field.dart
@@ -30,6 +30,8 @@ class CustomTextField extends StatelessWidget{
         Expanded(
           flex: isTime ? 0 : 1,
           child: TextFormField(
+            onSaved: onSaved,
+            validator: validator,
             cursorColor: Colors.grey,
             maxLines:  isTime ? 1 : null,
             //시간 관련 텍스트 필드가 아니면 한 줄 이상 작성 가능
