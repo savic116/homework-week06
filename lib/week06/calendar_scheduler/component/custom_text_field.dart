6aef37bda82baa326cea546c449bce6b8479cfd5
savic116 <savic00085@gmail.com>
2025-10-11 15:18:27 +0900

    feat: Implement basic calendar scheduler UI

---
+++ lib/week06/calendar_scheduler/component/custom_text_field.dart
@@ -0,0 +1,52 @@
+import 'package:myapp/week05/calendar_scheduler/const/colors.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+class CustomTextField extends StatelessWidget{
+  final String label; // 텍스트 필드 제목
+  final bool isTime; // 시간 선택하는 텍스트 필드인지 여부
+
+  const CustomTextField({
+    required this.label,
+    required this.isTime,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          label,
+          style: TextStyle(
+            color: PRIMARY_COLOR,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        Expanded(
+          flex: isTime ? 0 : 1,
+          child: TextFormField(
+            cursorColor: Colors.grey,
+            maxLines:  isTime ? 1 : null,
+            //시간 관련 텍스트 필드가 아니면 한 줄 이상 작성 가능
+            expands: !isTime,
+            keyboardType:  isTime ? TextInputType.number : TextInputType.multiline,
+            // 시간 관련 텍스트 필드는 기본 숫자 키보드 아니면 빌반 글자 키보드 보여주기
+            inputFormatters: isTime
+               ? [
+                FilteringTextInputFormatter.digitsOnly,
+               ]
+                 : [], 
+               decoration: InputDecoration(
+                border: InputBorder.none,
+                filled: true,
+                fillColor: Colors.grey[300],
+                suffixText: isTime ? '시' : null,
+               ),
+          ),
+        ),
+        TextFormField(), // 폼 안에서 텍스트 필드를 쓸 때 사용
+      ],
+    );
+  }
+}
---
+++ lib/week06/calendar_scheduler/main.dart
@@ -0,0 +1,16 @@
+import 'package:myapp/week05/calendar_scheduler/screen/home_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/date_symbol_data_local.dart';
+
+void main() async {
+  // 플러터 프레임워크가 준비될 때까지 대기
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await initializeDateFormatting(); // intl 패키지 초기화(다국어화)
+
+  runApp(
+    MaterialApp(
+      home: HomeScreen(),
+    ),
+  );
+}
