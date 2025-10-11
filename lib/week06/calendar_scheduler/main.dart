f7833e3b0f279f48e02eda1f3b0dbde9eb96aad1
savic116 <savic00085@gmail.com>
2025-10-11 16:26:28 +0900

    Fix: Correct `get_it` import statement

--- lib/week06/calendar_scheduler/main.dart
+++ lib/week06/calendar_scheduler/main.dart
@@ -3,7 +3,7 @@ import 'package:myapp/week05/calendar_scheduler/screen/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:myapp/week06/calendar_scheduler/database/drift_database.dart';
-import 'package: get_it/get_it.dart';
+import 'package:get_it/get_it.dart';
 
 void main() async {
   // 플러터 프레임워크가 준비될 때까지 대기
