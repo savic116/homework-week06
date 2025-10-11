d497516a25983c2d6aee364c8327e6c87723ecb8
savic116 <savic00085@gmail.com>
2025-10-11 14:53:34 +0900

    feat(text_field): Implement digit-only input for time fields

--- lib/week05/calendar_scheduler/component/custom_text_field.dart
+++ lib/week05/calendar_scheduler/component/custom_text_field.dart
@@ -32,7 +32,12 @@ class CustomTextField extends StatelessWidget{
             expands: !isTime,
             keyboardType:  isTime ? TextInputType.number : TextInputType.multiline,
             // 시간 관련 텍스트 필드는 기본 숫자 키보드 아니면 빌반 글자 키보드 보여주기
-            input
+            inputFormatters: isTime
+               ? [
+                FilteringTextInputFormatter.digitsOnly,
+               ]
+                 : [], 
+                
           ),
         ),
         TextFormField(), // 폼 안에서 텍스트 필드를 쓸 때 사용
