09e71ea5965f2653476156f5906075252b559e6a
savic116 <savic00085@gmail.com>
2025-10-11 15:12:34 +0900

    feat: Implement basic calendar scheduler app

---
+++ lib/week05/calendar_scheduler/main.dart
@@ -0,0 +1,8 @@
+import 'package:myapp/week05/calendar_scheduler/screen/home_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/date_symbol_data_local.dart';
+
+void main() async {
+  // 플러터 프레임워크가 준비될 때까지 대기
+  WidgetFlu
+}
