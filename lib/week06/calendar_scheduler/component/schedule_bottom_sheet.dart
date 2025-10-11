2b4526b9a7794fb5520aeb342cf733d3ac2266cc
savic116 <savic00085@gmail.com>
2025-10-11 15:56:26 +0900

    feat(schedule): Save start time input to variable

--- lib/week06/calendar_scheduler/component/schedule_bottom_sheet.dart
+++ lib/week06/calendar_scheduler/component/schedule_bottom_sheet.dart
@@ -34,6 +34,10 @@ class _ScheduleBottomSheetState extends State<ScheduleBottomSheet>{
                       child: CustomTextField(
                         label: '시작 시간',
                         isTime: true,
+                        onSaved: (String? val) {
+                          // 저장이 실행되면 startTime 변수에 텍스트 필드값 저장
+                          startTime = int.parse(val!);
+                        }
                       ),
                     ),
                     const SizedBox(width: 16.0),
