4b78ef422c6b6bdc61baa7f437787ffb14b2fb23
savic116 <savic00085@gmail.com>
2025-10-11 14:50:04 +0900

    feat(textfield): Implement time/content text field

--- lib/week05/calendar_scheduler/component/custom_text_field.dart
+++ lib/week05/calendar_scheduler/component/custom_text_field.dart
@@ -23,6 +23,13 @@ class CustomTextField extends StatelessWidget{
             fontWeight: FontWeight.w600,
           ),
         ),
+        Expanded(
+          flex: isTime ? 0 : 1,
+          child: TextFormField(
+            cursorColor: Colors.grey,
+            maxLines:  isTime ? 1 : null,
+          ),
+        ),
         TextFormField(), // 폼 안에서 텍스트 필드를 쓸 때 사용
       ],
     );
