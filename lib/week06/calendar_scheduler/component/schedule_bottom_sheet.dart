76420409cf1d3d1d546dc6140665bb3f50e3093c
savic116 <savic00085@gmail.com>
2025-10-11 16:18:57 +0900

    Refactor: Move validators outside of onSavePressed

--- lib/week06/calendar_scheduler/component/schedule_bottom_sheet.dart
+++ lib/week06/calendar_scheduler/component/schedule_bottom_sheet.dart
@@ -85,33 +85,9 @@ class _ScheduleBottomSheetState extends State<ScheduleBottomSheet>{
      ); 
     }
     void onSavePressed(){
-      String? timeValidator(String? val) {
-        if(val == null){
-          return '값을 입력해주세요';
-        }
+      String? timeValidator(String? val) {}
 
-        int? number;
-
-        try{
-          number = int.parse(val);
-        } catch (e) {
-          return '숫자를 입력해주세요';
-        }
-
-        if (number < 0 || number > 24) {
-          return '0시부터 24시 사이를 입력해주세요';
-        }
-        return null;
-      }
-
-      String? contentValidator(String? val){
-        if(val == null || val.length == 0){
-            return '값을 입력해주세요';
-          }
-
-        return null;
-
-      }
+      String? contentValidator(String? val){}
       if(formKey.currentState!.validate()){
         formKey.currentState!.save();
 
@@ -123,5 +99,30 @@ class _ScheduleBottomSheetState extends State<ScheduleBottomSheet>{
     }
 }
 
+String? timeValidator(String? val) {
+  if(val == null){
+    return '값을 입력해주세요';
+  }
 
+  int? number;
+
+  try{
+    number = int.parse(val);
+  } catch (e) {
+    return '숫자를 입력해주세요';
+  }
+
+  if (number < 0 || number > 24) {
+    return '0시부터 24시 사이를 입력해주세요';
+  }
+  return null;
+}
+
+String? contentValidator(String? val) {
+  if(val == null || val.length == 0){
+    return '값을 입력해주세요';
+  }
+
+  return null;
+}
 
