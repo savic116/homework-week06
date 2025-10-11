abf4926ac2fd914ea9f1d91060b2360bf5a3972d
savic116 <savic00085@gmail.com>
2025-10-11 15:23:56 +0900

    Fix: Corrected 'content' typo and added missing columns to Schedule model.

--- lib/week06/calendar_scheduler/model/schedule.dart
+++ lib/week06/calendar_scheduler/model/schedule.dart
@@ -2,6 +2,9 @@ import 'package:drift/drift.dart';
 
 class Schedules extends Table{
   IntColumn get id => integer().autoIncrement()();
-  TextColumn get contnet => text();
-  
+  TextColumn get contnet => text()();
+  DateTimeColumn get date => dateTime()();
+  IntColumn get startTime => integer()();
+  I
+
 }
