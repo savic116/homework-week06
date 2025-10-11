1993afd20a3c7201bb62cfd8f0d9624a4b985b59
savic116 <savic00085@gmail.com>
2025-10-11 15:43:26 +0900

    feat: Import calendar_scheduler module

--- lib/week06/calendar_scheduler/main.dart
+++ lib/week06/calendar_scheduler/main.dart
@@ -1,6 +1,7 @@
 import 'package:myapp/week05/calendar_scheduler/screen/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
+import 'package:myapp/week06/calendar_scheduler/'
 
 void main() async {
   // 플러터 프레임워크가 준비될 때까지 대기
