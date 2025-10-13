c54b440c1725c0a6c8e1eb6c1260f0a20197e904
savic116 <savic00085@gmail.com>
2025-10-13 10:13:53 +0900

    Fix: Update HomeScreen import path for week 06

--- lib/week06/calendar_scheduler/main.dart
+++ lib/week06/calendar_scheduler/main.dart
@@ -1,5 +1,5 @@
 import 'package:get_it/get_it.dart';
-import 'package:myapp/week05/calendar_scheduler/screen/home_screen.dart';
+import 'package:myapp/week06/calendar_scheduler/screen/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:myapp/week06/calendar_scheduler/database/drift_database.dart';
