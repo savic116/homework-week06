19efab50e27c506e4fd09ceae63b39d7ddfe3088
savic116 <savic00085@gmail.com>
2025-10-16 12:52:05 +0900

    feat: Add DriftDB import to schedule bottom sheet

--- lib/week06/calendar_scheduler/component/schedule_bottom_sheet.dart
+++ lib/week06/calendar_scheduler/component/schedule_bottom_sheet.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:myapp/week06/calendar_scheduler/component/custom_text_field.dart';
 import 'package:myapp/week06/calendar_scheduler/const/colors.dart';
+import 'package:myapp/week06/calendar_scheduler/database/drift_database.dart';
 class ScheduleBottomSheet extends StatefulWidget{
   final DateTime selectedDate;  // 선택된 날짜 상위 위젯
   const ScheduleBottomSheet({
