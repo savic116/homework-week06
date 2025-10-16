028ff7acd15cf2348f833c47f2104ca56993ec03
savic116 <savic00085@gmail.com>
2025-10-16 13:14:34 +0900

    feat: Implement schedule list rendering on home screen

--- lib/week06/calendar_scheduler/screen/home_screen.dart
+++ lib/week06/calendar_scheduler/screen/home_screen.dart
@@ -65,11 +65,23 @@ Widget build(BuildContext context) {
                   return Container();
                 }
                 return ListView.builder(
-                  
-                )
+                  // 리스트에 입력할 값들의 총 개수
+                  itemCount: snapshot.daya!.length,
+                  itemBuilder: (context, index) {
+                    final schedule = snapshot.data![index];
+                    return Padding(
+                      padding: const EdgeInsets.only(bottom: 8.0, left: 8.0, right:  8.0),
+                      child: ScheduleCard(
+                        startTime: schedule.startTime,
+                        endTime: schedule.endTime,
+                        content: schedule.content,
+                      ),
+                    );
+                  },
+                );
               }
             ),
-          )
+          ),
           ScheduleCard(
             startTime: 12,
             endTime: 14,
